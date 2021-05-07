6a8aae09c0c27864fa61db8e3e5b3620de719e81
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-07 20:23:46 +0200

    report issue drag

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,23 +2,17 @@ $(() => {
     $('.cnt .cnt-settings .close-menu img').click((e) => {
         $('.cnt-settings').hide();
         $('.cnt .cnt-map #menu-btn').show();
-        $('#poi-draggable').css('top', '75px');
-        $('#poi-draggable').css('left', '20px');
-        $('#poi-delete-draggable').css('top', '75px');
-        $('#poi-delete-draggable').css('left', '192px');
-        $('#bookmarks-draggable').css('top', '245px');
-        $('#bookmarks-draggable').css('left', '20px');
+        $('#poi-draggable').css({ 'top': '75px', 'left': '20px' });
+        $('#poi-delete-draggable').css({ 'top': '75px', 'left': '192px' });
+        $('#bookmarks-draggable').css({ 'top': '245px', 'left': '20px' });
     });
 
     $('.cnt .cnt-map #menu-btn').click((e) => {
         $('.cnt-settings').show();
         $('.cnt .cnt-map #menu-btn').hide();
-        $('#poi-draggable').css('top', '20px');
-        $('#poi-draggable').css('left', '20px');
-        $('#poi-delete-draggable').css('top', '20px');
-        $('#poi-delete-draggable').css('left', '192px');
-        $('#bookmarks-draggable').css('top', '190px');
-        $('#bookmarks-draggable').css('left', '20px');
+        $('#poi-draggable').css({ 'top': '20px', 'left': '20px' });
+        $('#poi-delete-draggable').css({ 'top': '20px', 'left': '192px' });
+        $('#bookmarks-draggable').css({ 'top': '190px', 'left': '20px' });
     });
 
     $('#collapsePOI li').not(':first').click((e) => {
