3a761fb2a4a0943be326ae76cf0af8229abfbaf4
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-13 16:46:40 +0200

    bright-contrast-control

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 initMap = () => {
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementsByClassName("map-sec")[0], {
         zoom: 4,
         center: { lat: 52, lng: 4 },
         disableDefaultUI: true,
