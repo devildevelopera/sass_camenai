5c348631875ba5ba08aa083c365eada7ad9ebe27
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-14 06:08:23 +0200

    resize complete

--- js/resize.js
+++ js/resize.js
@@ -1,19 +1,19 @@
 $(() => {
-    const BORDER_SIZE = 4;
-    const panel = document.getElementById("right_panel");
+    const resize_handler = document.getElementById("resize-handler");
+    const img_sec = document.getElementsByClassName("img-sec")[0];
+    const map_sec = document.getElementsByClassName("map-sec")[0];
 
     let m_pos;
     function resize(e) {
         const dy = e.y - m_pos;
         m_pos = e.y;
-        panel.style.height = (parseInt(getComputedStyle(panel, '').height) + dy) + "px";
+        img_sec.style.height = (parseInt(getComputedStyle(img_sec, '').height) + dy) + "px";
+        map_sec.style.height = (parseInt(getComputedStyle(map_sec, '').height) - dy) + "px";
     }
 
-    panel.addEventListener("mousedown", function (e) {
-        if ((parseInt(getComputedStyle(panel, '').height) - e.offsetY < BORDER_SIZE)) {
-            m_pos = e.y;
-            document.addEventListener("mousemove", resize, false);
-        }
+    resize_handler.addEventListener("mousedown", function (e) {
+        m_pos = e.y;
+        document.addEventListener("mousemove", resize, false);
     }, false);
 
     document.addEventListener("mouseup", function () {
