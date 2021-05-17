0912115b0295893147b587f41abc70f5500fb0f9
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-17 12:18:55 +0200

    vertical slider tooptip

--- js/vertical-slider.js
+++ js/vertical-slider.js
@@ -1,6 +1,19 @@
-var slider = new Slider("#brightness-vertical-slider", {
-    orientation: 'vertical'
-});
-var slider = new Slider("#contrast-vertical-slider", {
-    orientation: 'vertical'
+$().ready(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var slider = new Slider("#brightness-vertical-slider", {
+        orientation: 'vertical',
+        reversed: true,
+        formatter: function(value) {
+            return 'Brightness: ' + value + '%';
+        },
+    });
+
+    var slider = new Slider("#contrast-vertical-slider", {
+        orientation: 'vertical',
+        reversed: true,
+        formatter: function(value) {
+            return 'Contrast: ' + value + '%';
+        },
+    });
 });
