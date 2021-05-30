fff55aab0f4ea24fcb64f32a251c17ecb1d7a114
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-30 11:44:49 +0200

    slider track after

--- js/horizontal-slider.js
+++ js/horizontal-slider.js
@@ -2,7 +2,7 @@ $().ready(() => {
 
     setTimeout(() => {
         $(".slider.slider-horizontal .tooltip.tooltip-main.top .tooltip-arrow").append('<div class="inner-triangle"></div>');
-    }, 1000);
+    }, 500);
 
 
     var slider = new Slider("#image-quality-horizontal-slider", {
