99dcd911f7777226b2b666376047cc5ff32d7de7
Devil Developer <smartwebcoding12345@gmail.com>
2021-05-31 17:16:14 +0200

    street dimension control toggle

--- js/street-dimention-control.js
+++ js/street-dimention-control.js
@@ -1,9 +1,33 @@
 $(() => {
-    $('#street-map-toggle').on('click', function() {
+    $('#street-dimension-control div:first-child').on('click', function() {
         if ($(this).html() === 'Street') {
             $(this).html('Map');
         } else {
             $(this).html('Street');
         }
-    })
+    });
+
+    $('#street-dimension-control div:nth-child(2)').on('click', function() {
+        if ($(this).attr("class") === 'selected') {
+            $(this).removeClass('selected');
+        } else {
+            $(this).addClass('selected');
+        }
+    });
+
+    $('#street-dimension-control div:nth-child(3)').on('click', function() {
+        if ($(this).attr("class") === 'selected') {
+            $(this).removeClass('selected');
+        } else {
+            $(this).addClass('selected');
+        }
+    });
+
+    $('#street-dimension-control div:last-child').on('click', function() {
+        if ($(this).attr("class") === 'selected') {
+            $(this).removeClass('selected');
+        } else {
+            $(this).addClass('selected');
+        }
+    });
 })
