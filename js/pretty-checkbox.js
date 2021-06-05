deb694ba92442104663271d9faa0bce03a40273c
Devil Developer <smartwebcoding12345@gmail.com>
2021-06-05 17:15:00 +0200

    pixel perfect

--- js/pretty-checkbox.js
+++ js/pretty-checkbox.js
@@ -2,12 +2,12 @@ $(() => {
 
     setTimeout(() => {
         $(`.image-quality-horizontal-slider .slider.slider-horizontal .slider-handle.min-slider-handle.round`).show();
-        $(`.image-quality-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
+        $(`.image-quality-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
         $(`.image-quality-horizontal-slider .slider.slider-horizontal .slider-track`).addClass('selected');
         $(`.image-quality-horizontal-slider .slider.slider-horizontal .tooltip`).show();
 
         $(`.gps-accuracy-horizontal-slider .slider.slider-horizontal .slider-handle.min-slider-handle.round`).show();
-        $(`.gps-accuracy-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
+        $(`.gps-accuracy-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
         $(`.gps-accuracy-horizontal-slider .slider.slider-horizontal .slider-track`).addClass('selected');
         $(`.gps-accuracy-horizontal-slider .slider.slider-horizontal .tooltip`).show();
     }, 500);
@@ -15,11 +15,11 @@ $(() => {
     $('.image-filter-item .pretty input').on('click', (e) => {
         if (!e.target.checked) {
             $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-handle.min-slider-handle.round`).hide();
-            $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg")');
+            $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg"), url("imgs/SVG/vertical-line-hidden.svg")');
             $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .tooltip`).hide();
         } else {
             $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-handle.min-slider-handle.round`).show();
-            $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
+            $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-track`).css('background-image', 'url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg"), url("imgs/SVG/vertical-line.svg")');
             $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .tooltip`).show();
         }
         $(`.${e.target.name}-horizontal-slider .slider.slider-horizontal .slider-track`).toggleClass('selected');
